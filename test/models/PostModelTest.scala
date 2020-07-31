461ade7a599239c6269d37fc57e75ca79e04f8a8
Quang Ha Bui <ha_bq@septeni-technology.jp>
2020-07-31 10:58:11 +0700

    fix testcase

--- test/models/PostModelTest.scala
+++ test/models/PostModelTest.scala
@@ -7,7 +7,7 @@ class PostModelTest extends Specification with db.DbTest {
   val post = new PostDAO
   initTestDB()
 
-  // test pass: list 2 posts
+  // test fail: list 2 posts
   "PostDao" should {
     "getAll() return a Success(List(Post))" in {
       val postList = post.getAll.get
@@ -15,7 +15,7 @@ class PostModelTest extends Specification with db.DbTest {
     }
   }
 
-  // test fail: list 6 posts
+  // test pass: list 2 posts
   "PostDao" should {
     "getAll() return a Success(List(Post))" in {
       val postList = post.getAll.get
